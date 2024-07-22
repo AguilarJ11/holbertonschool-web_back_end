e6dd67bc9af193ba9f3d11eda0191512c2f5c165
Joaquin <aguilarj4126@gmail.com>
2024-07-22 19:06:06 -0300

    exercise 5 done?

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    }
+  }
+
+  static evacuationWarningMessage() {
+  }
+}
