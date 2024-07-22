01f69dba75f76b69d60c6cdd67a1c242e47f80fe
Joaquin <aguilarj4126@gmail.com>
2024-07-22 17:54:05 -0300

    exercise 4 done

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,36 +1,36 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor () {
-        this._amount = this._amount
-        this._currency = currency
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
     }
+  }
 
-    get amount() {
-        return this._amount;
-      }
-    
-      set amount(amount) {
-        if (typeof amount === 'number') {
-          this._amount = amount;
-        }
-      }
-    
-      get currency() {
-        return this._currency;
-      }
-    
-      set currency(currency) {
-        if (currency instanceof Currency) {
-          this._currency = currency;
-        }
-      }
-      
-      displayFullPrice() {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-      }
+  get currency() {
+    return this._currency;
+  }
 
-      static convertPrice(amount, convertPrice) {
-        return amount * convertPrice
-      }
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, convertPrice) {
+    return amount * convertPrice;
+  }
 }
