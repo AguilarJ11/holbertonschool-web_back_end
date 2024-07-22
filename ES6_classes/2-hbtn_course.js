c3c3f4b90f787302867ff5d6cf899630bdd49b34
Joaquin <aguilarj4126@gmail.com>
2024-07-22 14:53:02 -0300

    asnf

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -12,9 +12,8 @@ export default class HolbertonCourse {
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
-    }
-    else {
-        throw new TypeError('Name must be a string')
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
 
@@ -25,9 +24,8 @@ export default class HolbertonCourse {
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
-    }
-    else {
-        throw new TypeError('Length must be a number')
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
 
@@ -36,7 +34,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (Array.isArray(students) || students.every((elem) => typeof elem === 'string')) {
+    if (
+      Array.isArray(students) || students.every((elem) => typeof elem === 'string')
+    ) {
       this._students = students;
     }
   }
