a95184a89ae88b62170b86f7008bdfa1816ac58b
Joaquin <aguilarj4126@gmail.com>
2024-07-23 16:32:32 -0300

    change object name and atribute

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 export default function getListStudents() {
-  function Students(id, firstname, location) {
+  function Object(id, firstName, location) {
     this.id = id;
-    this.firstname = firstname;
+    this.firstName = firstName;
     this.location = location;
   }
   const students = [];
-  const Guillaume = new Students(1, 'Guillaume', 'San Francisco');
-  const Jamese = new Students(2, 'James', 'Columbia');
-  const Serena = new Students(5, 'Serena', 'San Francisco');
+  const Guillaume = new Object(1, 'Guillaume', 'San Francisco');
+  const Jamese = new Object(2, 'James', 'Columbia');
+  const Serena = new Object(5, 'Serena', 'San Francisco');
   students.push(Guillaume, Jamese, Serena);
 
   return students;
