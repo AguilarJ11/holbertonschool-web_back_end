aef4cfe9f83bd5768c3c69596458bf4e34adfc73
Joaquin <aguilarj4126@gmail.com>
2024-07-23 16:51:36 -0300

    exercise 1 done

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,8 @@
-export default function getListStudentIds() {
-    
+export default function getListStudentIds(array) {
+  let idArray = [];
+  if (Array.isArray(array)) {
+    idArray = array.map((student) => student.id);
+    return idArray;
+  }
+  return idArray;
 }
