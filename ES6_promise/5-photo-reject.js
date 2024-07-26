c94f36cdd82fac10fa4d10aa82c4a5e4a711b7dc
Joaquin <aguilarj4126@gmail.com>
2024-07-26 16:23:19 -0300

    fix

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,8 +1,5 @@
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    resolve();
-    if (fileName) {
-      reject(Error(`${fileName} cannot be processed`));
-    }
+    reject(Error(`${fileName} cannot be processed`));
   });
 }
