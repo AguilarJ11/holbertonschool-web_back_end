690d0bd1d4ce1c02f8689d98b9dd4918606a9798
Joaquin <aguilarj4126@gmail.com>
2024-07-26 14:40:21 -0300

    idk

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-        const [photo, user] = values
+      const [photo, user] = values;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     }), () => {
     console.log('Signup system offline');
