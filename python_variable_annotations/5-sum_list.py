d9671b9811ddf48cd4e4b9d42aac02aabca20dd1
Joaquin <aguilarj4126@gmail.com>
2024-08-13 15:56:43 -0300

    fix=?

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 """function that returns their sum as a float"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """function that returns their sum as a float"""
     res: float = 0.0
     for num in input_list:
