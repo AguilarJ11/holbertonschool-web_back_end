f64da4b3df4d347586ac3a3223b886aea6233ca4
Joaquin <aguilarj4126@gmail.com>
2024-08-13 15:47:49 -0300

    exercise 5 done

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""function that returns their sum as a float"""
+
+def sum_list(input_list: list[float]) -> float:
+    """function that returns their sum as a float"""
+    res: float = 0
+    for num in input_list:
+        res =+ num
+    return res
