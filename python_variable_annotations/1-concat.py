af0c9bf891383ce27ab226e9b6f8aca736f0c351
Joaquin <aguilarj4126@gmail.com>
2024-08-13 15:14:42 -0300

    exercise 2 done

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Function for concatenate 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function for concatenate 2 strings"""
+    return str1 + str2
