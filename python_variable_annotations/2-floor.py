43e66e0ae8aaf7c803ddc04805ba406fe1085c4f
Joaquin <aguilarj4126@gmail.com>
2024-08-13 15:30:45 -0300

    exercise 2 done

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Function that returns the floor of the float"""
+
+
+def floor(n: float) -> int:
+    """Function that returns the floor of the float"""
+    return int(n)
