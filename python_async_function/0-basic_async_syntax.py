842e91f7f5dcedc42bc1e277feee803dce9c7838
Joaquin <aguilarj4126@gmail.com>
2024-08-14 12:51:21 -0300

    delete asyncio.run

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,5 +10,3 @@ async def wait_random(max_delay: int = 10) -> float:
     """Return a random number between 0 and max_delay(def 10)"""
     await asyncio.sleep(1)
     return print(random.uniform(0, max_delay))
-
-asyncio.run(wait_random())
