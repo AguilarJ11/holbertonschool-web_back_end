c49c83ef9c89426c84547452fd2dac7de2729620
Joaquin <aguilarj4126@gmail.com>
2024-08-14 15:08:34 -0300

    pycodestyle fix

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,8 +2,8 @@
 
 """Create a task an return it"""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
