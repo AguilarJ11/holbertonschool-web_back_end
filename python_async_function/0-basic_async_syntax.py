e876d746b9962cb64c3b980e7209958287d20763
Joaquin <aguilarj4126@gmail.com>
2024-08-14 12:59:00 -0300

    fix?

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+"""Return a random number between 0 and max_delay(def 10)"""
+
 import random
 import asyncio
-
-"""Return a random number between 0 and max_delay(def 10)"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
