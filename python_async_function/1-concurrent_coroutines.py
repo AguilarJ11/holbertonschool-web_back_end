7be54ad6adfde28e7450711ab1ef87a91a235be2
Joaquin <aguilarj4126@gmail.com>
2024-08-14 13:47:58 -0300

    exercise 1 done

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Execute n times the wait_random
+function with the max_delay for arg
+and return a list of those random numbers"""
+
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Execute n times the wait_random
+    function with the max_delay for arg
+    and return a list of those random numbers"""
+    random_list = []
+    for times in range(n):
+        random_list.append(await wait_random(max_delay))
+
+    random_list.sort()
+    return random_list
