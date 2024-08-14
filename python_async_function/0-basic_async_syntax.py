39114b5214ce2caa9c31a6f11886a6baa913780e
Joaquin <aguilarj4126@gmail.com>
2024-08-14 12:57:38 -0300

    fix?

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,5 +8,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """Return a random number between 0 and max_delay(def 10)"""
-    await asyncio.sleep(1)
-    return print(random.uniform(0, max_delay))
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
