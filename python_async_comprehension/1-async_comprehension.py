ed6165c07b8b0820bce84afc4abe5f050c388d67
Joaquin <aguilarj4126@gmail.com>
2024-08-21 19:05:16 -0300

    add anotations, documentation and change async loop for async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-""""""
+"""will collect 10 random numbers using an
+async comprehensing over async_generator"""
 
-async_generator = __import__('0-async_generator').async_generator
 import random
 import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    random_numbers = []
-    async for i in async_generator():
-        random_numbers.append(i)
-    return random_numbers
+async def async_comprehension() -> List[float]:
+    """will collect 10 random numbers using an
+    async comprehensing over async_generator"""
+    return [i async for i in async_generator()]
