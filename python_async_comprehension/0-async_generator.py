4e475eea15e6c773c36ad975838f4f650b4b171c
Joaquin <aguilarj4126@gmail.com>
2024-08-21 17:59:17 -0300

    exercise 0 done

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Loop 10 times and wait 1 second per loop, then yield a random number
+between 0 and 10"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """Loop 10 times and wait 1 second per loop, then yield a random number
+    between 0 and 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
