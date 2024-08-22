6bc9f2581b94e8da054e7f99b6fb6899783754f8
Joaquin <aguilarj4126@gmail.com>
2024-08-22 18:57:18 -0300

    exercise 0 done

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+"""return a tuple of size two containing
+a start index and an end index"""
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    pass
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing
+    a start index and an end index corresponding"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
