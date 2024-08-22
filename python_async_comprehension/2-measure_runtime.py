dc7aa5615ef86d015113b0efc1280dac085d2a35
Joaquin <aguilarj4126@gmail.com>
2024-08-22 18:16:14 -0300

    pycodestyle add

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -13,5 +13,5 @@ async def measure_runtime() -> float:
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.perf_counter()
     total_time = end_time - start_time
-    
+
     return total_time
