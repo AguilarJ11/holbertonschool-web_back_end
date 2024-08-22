3b48bd58f0c65c1194bf19f9b00ebd37f1d820ec
Joaquin <aguilarj4126@gmail.com>
2024-08-22 18:15:45 -0300

    exercise 2 done

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""will collect 10 random numbers using an
+async comprehensing over async_generator"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start_time = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    
+    return total_time
