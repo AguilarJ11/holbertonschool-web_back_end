f2e9b36bc709dc76e2f6975482f1047a94d88283
Joaquin <aguilarj4126@gmail.com>
2024-08-25 14:10:22 -0300

    pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,15 +36,15 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         file = self.dataset()
         start, end = index_range(page, page_size)
-        if len(data) > end:
+        if len(file) < end:
             return []
         data = file[start:end]
-        
+
         return data
