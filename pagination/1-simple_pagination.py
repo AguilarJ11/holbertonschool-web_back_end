7abc68848bb0c1afee2a4db0837a35ef235b8fb1
Joaquin <aguilarj4126@gmail.com>
2024-08-25 14:00:41 -0300

    add get_page as a method

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,14 +36,13 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-
-
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
-
-    file = self.dataset()
-    start, end = index_range(page, page_size)
-    data = file[start:end]
     
-    return data
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        file = self.dataset()
+        start, end = index_range(page, page_size)
+        data = file[start:end]
+        
+        return data
