8dc2b4589df78be034fb05af937b1e766f23a003
Joaquin <aguilarj4126@gmail.com>
2024-09-02 21:35:39 -0300

    fix function

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -10,6 +10,6 @@ def insert_school(mongo_collection, **kwargs):
     return the id of the created document
     """
     
-    document = mongo_collection.insertOne(kwargs)
+    document = mongo_collection.insert_one(kwargs)
     
     return document.inserted_id
