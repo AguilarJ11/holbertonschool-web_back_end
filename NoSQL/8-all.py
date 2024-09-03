046ddc5b81dc77e1b55368a1e8daa92612d4ffe6
Joaquin <aguilarj4126@gmail.com>
2024-09-02 21:25:54 -0300

    exercise 8 done

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+lists all documents in a collection
+"""
+
+import pymongo
+
+def list_all(mongo_collection):
+    collection_list = []
+    if mongo_collection.count_documents({}) == 0:
+        return collection_list
+    else:
+        collection_list = list(mongo_collection.find())
+        return collection_list
