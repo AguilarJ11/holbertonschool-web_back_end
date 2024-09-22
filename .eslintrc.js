b2402747e22a88266c62e1acd40814999629d9e8
Joaquin <aguilarj4126@gmail.com>
2024-09-22 19:13:19 -0300

    idk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,32 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+  env: {
+    es6: true,
+    node: true, // Agregue node:true para evitar posibles problemas en entornos Node.js
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended', // Se agrega una configuración básica recomendada
+    'airbnb-base', // Sigue usando airbnb-base
+    'plugin:jest/recommended', // Cambié a recommended para mayor compatibilidad
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Actualicé a 2020 para mayor compatibilidad con las nuevas características de JS
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off', // Permite console.log para depuración
+    'no-shadow': 'off', // Desactiva la regla de sombras
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement', // Permite etiquetas
+      'WithStatement', // Permite with
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
+  ],
+};
